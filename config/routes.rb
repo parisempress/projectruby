19828ae1e6fce70e8fd5f6cee23d566981c37658
Rochelle Lawrence <parisempress@gmail.com>
2016-05-15 17:38:00 +0000

    contact and email

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,13 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   
   get 'static_pages/landing_page'
+  
+   post 'static_pages/thank_you'
  
   get 'static_pages/index'
-
+  
+ 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
