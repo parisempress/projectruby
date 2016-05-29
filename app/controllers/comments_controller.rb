20b0c8b237192898704ae1161de8c2f02c75d009
Rochelle Lawrence <parisempress@gmail.com>
2016-05-29 14:20:04 +0000

    k

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,32 +1,22 @@
 class CommentsController < ApplicationController
 def create
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.new(comment_params)
-    @comment.user = current_user
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-        
-      end
-    end
-        
-
+  @product = Product.find(params[:product_id])
+  @comment = @product.comments.new(comment_params)
+  @comment.user = current_user
+  @comment.save
+  redirect_to product_path(@product)
+end
 
   def destroy
   end
   
-  def show
+def show
     @comments = @post.comments
     respond_with(@comments)
-  end
+end
 private
 
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
-end
 end
